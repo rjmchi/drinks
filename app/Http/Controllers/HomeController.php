6e0a6fc02a2ece1e37082812b197b20ca2170909
rjmchi <robert@rjmprogramming.com>
2023-04-01 13:56:02 -0600

    Adds pagination

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,25 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use App\Models\Drink;
 use PhpParser\Node\Stmt\Catch_;
 
 class HomeController extends Controller
 {
     public function index($category=null) {
         $data['catlist'] = Category::orderBy('name')->get();
-        if ($category) {
-            $data['categories']= Category::with(['drinks'=> function($query){
-                $query->orderBy('name');
-            }])->where('slug', $category)->get();
-        } else {
-            $data['categories'] = Category::with(['drinks'=> function($query){
-                $query->orderBy('name');
-            }])->orderBy('name')->get();
-        }
-        $data['selected'] = $category;
+
         if ($category){
-            $cat = Category::where ('slug', $category)->first();
-            $data['selected'] = ($cat->name);
+            $cat = Category::where('slug', $category)->first();
+            $data['selected'] = $cat->name;
+            $data['drinks'] = Drink::orderBy('name')->where('category_id', $cat->id)->paginate(20);
+        } else {
+            $data['drinks'] = Drink::orderBy('category_id')->orderBy('name')->paginate(20);
+            $data['selected'] = null;
         }
         return view('home')->with($data);
     }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,15 +11,16 @@
         </div>
     </div>
 <div class="container">
-    @if ($selected)
+@if ($selected)
         <h1 class="text-3xl bold text-center">{{$selected}}</h1>
     @endif
-    @foreach($categories as $category)
-        @foreach($category->drinks as $drink)
+
+    @if(count($drinks) > 0)
+        @foreach($drinks as $drink)
             <div class="drink flex flex-wrap justify-between border-2 border-slate-500 rounded-md m-3 text-lg">
                 <div class="title flex justify-between p-2 basis-full">
                     <span class="drink-name text-2xl font-bold p-2 basis-1/3">{{$drink->name}}</span>
-                    <span class="category font-bold p-2">{{$category->name}}</span>
+                    <span class="category font-bold p-2">{{$drink->category->name}}</span>
                     <span class="glass p-2 basis-1/4 text-right">{{$drink->glass}}</span>
                 </div>
                 <span class="build p-2">{{$drink->method->method}}</span>
@@ -33,6 +34,9 @@
                 <span class="garnish p-2 basis-1/4 text-center self-end">Garnish: {{$drink->garnish}}</span>
             </div>
         @endforeach
-    @endforeach
+        {!! $drinks->links() !!}
+    @else
+        <p class="mt-4 text-center">No Drinks Found</p>
+    @endif
     </div>
 </x-guest-layout>
