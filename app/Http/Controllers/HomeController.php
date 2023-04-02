c633c33d45ef89e6efaa325e0a8b6e179ebf27fd
rjmchi <robert@rjmprogramming.com>
2023-04-02 12:13:29 -0600

    Changes Sort

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,7 +17,7 @@ class HomeController extends Controller
             $data['selected'] = $cat->name;
             $data['drinks'] = Drink::orderBy('name')->where('category_id', $cat->id)->paginate(20);
         } else {
-            $data['drinks'] = Drink::orderBy('category_id')->orderBy('name')->paginate(20);
+            $data['drinks'] = Drink::orderBy('name')->paginate(20);
             $data['selected'] = null;
         }
         return view('home')->with($data);
